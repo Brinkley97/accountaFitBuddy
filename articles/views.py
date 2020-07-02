5141c80cad306d69e354db8bee69416e2889d903
Detravious Brinkley <d.brinkley97@gmail.com>
2020-07-02 15:13:23 -0400

    updated the article_detail

--- articles/views.py
+++ articles/views.py
@@ -6,6 +6,7 @@ from .import forms
 import os
 # to stop the code and view in terminal use print(text here)pdb.set_trace()
 import pdb
+from django.utils.datastructures import MultiValueDictKeyError
 
 
 
@@ -36,12 +37,15 @@ def article_create(request):
             for f in files:
                 instance = form.save(commit=False)
                 instance.author = request.user
-                imageFile = request.FILES['image'].name
-                imageExtention = os.path.splitext(imageFile)[1]
-                if imageExtention == '.jpeg':
-                    instance.videoFile = os.path.splitext(imageFile)[0] + '.jpg'
-                    instance.save()
-
+                try:
+                    imageFile = request.FILES['image'].name
+                    imageExtention = os.path.splitext(imageFile)[1]
+                    if imageExtention == '.jpeg':
+                        instance.videoFile = os.path.splitext(imageFile)[0] + '.jpg'
+                        instance.save()
+                except MultiValueDictKeyError:
+                    image = False
+                    
                 videoFile = request.FILES['video'].name
                 extention = os.path.splitext(videoFile)[1]
                 if extention == '.MOV':
