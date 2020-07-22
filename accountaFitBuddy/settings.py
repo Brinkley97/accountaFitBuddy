c0bb44418851a2c167f3e40d5b5245cbd60e0647
Detravious Brinkley <d.brinkley97@gmail.com>
2020-07-21 22:45:14 -0400

    updated login page

--- accountaFitBuddy/settings.py
+++ accountaFitBuddy/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets/css'),
     os.path.join(BASE_DIR, 'assets/img'),
     os.path.join(BASE_DIR, 'assets/js'),
+    os.path.join(BASE_DIR, 'assets/login'),
     )
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
